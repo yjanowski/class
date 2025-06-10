30072d66a06ba04ef25f135fdf360723ff72aaa9
yjanowski <ylj254@gmail.com>
2025-06-09 22:18:23 -0400

    Homework 49 - Quiz

---
+++ Html/49/Shirt.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShirtCreator
+    {
+    internal class Shirt
+        {
+        public Shirt(string color, string style) 
+            {
+            Color = color;
+            Style = style;
+            }
+        public string Color { get; set; }
+        public string Style { get; set; }
+
+
+        
+        }
+    }
+    
---
+++ Html/49/ShirtCreator.cs
@@ -0,0 +1,23 @@
+﻿namespace ShirtCreator
+    {
+    internal class ShirtCreator
+        {
+        static void Main(string[] args)
+            {
+            string[] colors = { "Red", "Green", "Blue" };
+            string[] styles = { "Striped", "Checked", "Plain" };
+            List<Shirt> shirtList = new List<Shirt>();
+            
+
+
+            for (int i = 0; i < colors.Length; i++)
+                {
+                for (int j = 0; j < styles.Length; j++)
+                    {
+                    shirtList.Add( new Shirt(colors[i], styles[j]));
+                    
+                    }
+                }
+            }
+        }
+    }
